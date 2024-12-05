0ac1f0945e823ccd36a28c2bb5c71af16359feb4
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-05 15:29:34 +0200

    Time: 11 ms (55.75%), Space: 21.7 MB (37.84%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.size();
+
+        int l = 0, r = 0;
+        while(l<n&&r<n){
+            while(r<n&&target[r]=='_')r++;
+            if(r==n)break;
+            char tar = target[r];
+            while(l<n&&start[l]=='_')l++;
+            if(l==n)break;
+            if(start[l]!=tar)break;
+            else{
+                if(tar=='R'&&l>r)return false;
+                if(tar=='L'&&l<r)return false;
+            }
+            l++,r++;
+        }
+        while(l<n&&start[l]=='_')l++;
+        while(r<n&&target[r]=='_')r++;
+
+        if(l==n&&r==n)return true;
+        else return false;
+    }
+};
