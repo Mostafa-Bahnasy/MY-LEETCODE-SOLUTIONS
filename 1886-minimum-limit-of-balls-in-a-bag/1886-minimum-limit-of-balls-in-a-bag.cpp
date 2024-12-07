d03e3b65528ed0398b6106fe0be04e4fd87c5b65
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-07 02:22:35 +0200

    Time: 51 ms (24.44%), Space: 59.8 MB (22.28%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        // auto solve = [&](auto&self,int n,int& threshold)->int{
+        //     if(n<=threshold)return 0;
+        //     int ret = 1;
+        //     int left = self(self,n/2,threshold);
+        //     int right =self(self,(1+n)/2,threshold);
+        //     left = min(left,((n/2+threshold-1)/threshold)-1);
+        //     right = min(right,(((n+1)/2+threshold-1)/threshold)-1);
+        //     ret = ret + left + right;
+        //     return ret;
+        // };
+
+        auto check = [&](int mx)->bool{
+            int need = 0;
+            for(auto&ele:nums){
+                need+=max(0,((ele+mx-1)/mx)-1);
+                //cout<<need<<"\n";
+                if(need>maxOperations)return false;
+            }
+            return true;
+        };
+        int l = 1, r = *max_element(nums.begin(),nums.end());
+        int ans = r;
+        while(l<=r){
+            int md = l+r>>1;
+            if(check(md))ans = md,r = md-1;
+            else l = md+1;
+        }
+        return ans;
+    }
+};
