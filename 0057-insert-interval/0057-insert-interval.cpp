c9941d4d6515c9f334e6e44b64bab886b941b715
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-08 16:58:00 +0200

    Time: 972 ms (5.4%), Space: 22.7 MB (5.12%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -6,9 +6,12 @@ public:
         sort(v.begin(),v.end());
         for(int i = 0;i<-1+v.size();i++){
             if(v[i][0]<=v[i+1][0]&&v[i][1]>=v[i+1][0]){
-                v[i][0] = min(v[i][0],v[i+1][0]);
-                v[i][1] = max(v[i][1],v[i+1][1]);
-                v.erase(v.begin()+1+i);
+                vector<int>tmp(2);
+                tmp[0]= min(v[i][0],v[i+1][0]);
+                tmp[1] = max(v[i][1],v[i+1][1]);
+                v.erase(v.begin()+i);
+                v.erase(v.begin()+i);
+                v.insert(v.begin()+i,tmp);
                 // break;
                 i--;
             }
