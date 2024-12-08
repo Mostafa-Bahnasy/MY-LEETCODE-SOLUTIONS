207a256d5713ab61d0b7a7e808c74e027a36f616
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-08 16:08:36 +0200

    Time: 92 ms (54.95%), Space: 137.2 MB (56.07%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -6,16 +6,18 @@ public:
         sort(left.begin(),left.end());
         
         int can = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> open;
+        unordered_map<int,int>mp;
+        priority_queue<int, vector<int>, greater<int>> open;
         int ans = 0;
         for(auto&v:left){
             //cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<"\n";
-            while(open.size()&&open.top().first<v[0]){
-                can= max(open.top().second,can);
+            while(open.size()&&open.top()<v[0]){
+                can= max(mp[open.top()],can);
                 open.pop();
             }
             ans = max(ans,v[2]+can);
-            open.push({v[1],v[2]});
+            open.push(v[1]);
+            mp[v[1]] = max(v[2],mp[v[1]]);
         }
         return ans;
 
