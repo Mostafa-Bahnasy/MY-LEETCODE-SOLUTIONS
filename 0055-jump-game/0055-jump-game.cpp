ba761f793ed1df9bc0aa1176fb5ccbdef1171bc6
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-09 15:19:32 +0200

    Time: 0 ms (100%), Space: 52.1 MB (27.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
         int n = nums.size();
-
-        queue<int>q;
-        vector<bool>vis(n+1);
-        q.push(0);
-        vis[0] = true;
-
-        while(q.size()){
-            auto tp = q.front();
-            q.pop();
-            int lim = nums[tp];
-            for(int i = 1;i<=lim&&tp+i<n;i++){
-                if(!vis[tp+i]){
-                    vis[tp+i] = true;
-                    q.push(tp+i);
-                }
-            }
+        int r = nums[0];
+        for(int i = 1;i<n;i++){
+            if(r>=i)r = max(r,i+nums[i]);
         }
-        return vis[n-1];
+        return r>=n-1;
     }
 };
