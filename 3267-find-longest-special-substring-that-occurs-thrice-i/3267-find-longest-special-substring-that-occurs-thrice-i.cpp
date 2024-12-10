3d5238e780df932a140d81d22f2c8196e1b1729b
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-10 19:17:49 +0200

    Time: 8 ms (75.95%), Space: 10.5 MB (87.41%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -4,7 +4,9 @@ public:
         
         int ans = -1;
         int n = s.size();
-        for(int len = 1;len<=n;len++){
+        int l = 1 , r = n;
+        while(l<=r){
+            int len = (l+r)/2;
             unordered_map<string , int>cnt;
             for(int i = 0;i+len-1<n;i++){
                 bool is = true;
@@ -14,13 +16,17 @@ public:
                 if(is)
                     cnt[s.substr(i,len)]++;
             }
+            bool can = false;
             for(auto&[ss,c]:cnt){
                 if(c>2){
                     cout<<ss<<"\n";
                     ans = len;
+                    can = true;
                     break;
                 }
             }
+            if (can == true)l = len+1;
+            else r= len-1;
         }
         return ans;
     }
