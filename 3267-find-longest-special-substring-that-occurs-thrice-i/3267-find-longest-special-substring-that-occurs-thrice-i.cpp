7284c611fd0214d7c9d753ce1f537ce15faa588a
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-10 19:23:25 +0200

    Time: 0 ms (100%), Space: 9 MB (96.76%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -7,25 +7,21 @@ public:
         int l = 1 , r = n;
         while(l<=r){
             int len = (l+r)/2;
-            unordered_map<string , int>cnt;
-            for(int i = 0;i+len-1<n;i++){
-                bool is = true;
-                for(int j = i;j<=i+len-1;j++){
-                    if(s[j]!=s[i]){is = false;break;}
+            int count[26] = {};
+            for(int i = 0;i<n;i++){
+                int cnt = 0;
+                int j = i;
+                while(j<n&&s[j]==s[i])cnt++,j++;
+
+                if(cnt>=len){
+                    count[(s[i]-'a')]+=(cnt-len+1);
                 }
-                if(is)
-                    cnt[s.substr(i,len)]++;
+                i = j-1;
             }
-            bool can = false;
-            for(auto&[ss,c]:cnt){
-                if(c>2){
-                    cout<<ss<<"\n";
-                    ans = len;
-                    can = true;
-                    break;
-                }
-            }
-            if (can == true)l = len+1;
+            //cout<<len<<" " <<*max_element(count,count+26)<<"\n";
+            bool can = (*max_element(count,count+26)>2);
+            
+            if (can == true)l = len+1,ans=len;
             else r= len-1;
         }
         return ans;
