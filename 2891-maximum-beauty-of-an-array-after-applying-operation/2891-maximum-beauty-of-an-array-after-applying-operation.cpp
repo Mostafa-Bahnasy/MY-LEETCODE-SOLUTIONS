d267eafa9cb3b49e928b758ab8da40b0ace4c1fb
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-11 18:52:25 +0200

    Time: 144 ms (35.65%), Space: 106.4 MB (49.1%) - LeetHub

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        int n = nums.size();
+        int prf[300010] = {};
+        int shft = 1e5;
+        for(auto&i:nums){
+            prf[i+shft+k+1]--;
+            prf[i+shft-k]++;
+        }
+        int ans = max(1,prf[0]);
+
+        for(int i = 1;i<=3e5+5;i++){
+            prf[i] = prf[i] + prf[i-1];
+            ans = max(ans,prf[i]);
+        }
+        return ans;
+    }
+};
