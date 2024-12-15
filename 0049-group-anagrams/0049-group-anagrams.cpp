e57bdf1d5e88fc78fa93d8d3c7c1ab5592289bc5
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-15 15:56:07 +0200

    Time: 19 ms (55.32%), Space: 24.5 MB (71.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        const long long mod=1e9+7;
-        long long base = 37;
-        long long pw[110] ={};
-        pw[0]=1;
-        for(int i= 1;i<105;i++)
-            pw[i] = (pw[i-1]*base)%mod;
+        
 
-        auto hash_it =[&](string&s)->long long{
-            long long ret = 0;
-            int i = 0;
-            for(auto& ch:s){
-                ret = ret + (((ch-'a')+1)*pw[i])%mod;
-                ret%=mod;
-                i++;
-            }
-            return ret;
-        };
-
-        unordered_map<long long,vector<int>>idx;
+        unordered_map<string,vector<int>>idx;
         int i = 0;
         for(auto s:strs){
             sort(s.begin(),s.end());
-            long long hsh = hash_it(s);
-            idx[hsh].push_back(i);
+            idx[s].push_back(i);
             i++;
         }
 
