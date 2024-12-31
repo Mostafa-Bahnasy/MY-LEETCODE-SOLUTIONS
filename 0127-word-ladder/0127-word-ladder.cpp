7c0907cf5ae060a5eb64a7e64cdff38ade63c55b
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2025-01-01 00:57:04 +0200

    Time: 137 ms (45.41%), Space: 74.4 MB (5%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,unordered_set<string>>all;
-        
-        for(auto&s:wordList){
-            auto ss = s;
+        unordered_map<string,unordered_set<int>>all;
+        unordered_map<string,int>idx;
+
+        wordList.push_back(endWord);
+        wordList.insert(wordList.begin(),beginWord);
+        for(int i = 0;i<wordList.size();i++){
+            // if(idx.count(wordList[i]))continue;
+            idx[wordList[i]] = i;
+        }
+        for(int i = 1;i<wordList.size()-1;i++){
+            auto ss = wordList[i];
+            auto s = wordList[i];
+            
             for(int i= 0;i<s.size();i++){
                 char old = s[i];
                 s[i] = '#';
-                all[s].insert(ss);
+                all[s].insert(idx[ss]);
                 s[i] = old;
             }
+            // i++;
         }
 
         // queue<int>q;
 
-        unordered_map<string,int>dst;
-        dst[beginWord] = 1;
-
-        queue<string>q;
-        q.push(beginWord);
+        unordered_map<int,int>dst;
+        dst[idx[beginWord]] = 1;
+        queue<int>q;
+        q.push(idx[beginWord]);
         while(q.size()){
-            auto s = q.front();
+            auto id = q.front();
             q.pop();
             // cout<<s<<"\n";
+            string s = wordList[id];
             if(s==endWord){
-                return dst[s];
+                return dst[id];
             }
-            int d = dst[s];
+            int d = dst[id];
             for(int i = 0;i<s.size();i++){
                 char old = s[i];
                 s[i] = '#';
