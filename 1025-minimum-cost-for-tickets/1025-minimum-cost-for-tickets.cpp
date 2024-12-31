a111654f0d8b60d730635f8726545133941585ab
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2024-12-31 02:58:04 +0200

    Time: 0 ms (100%), Space: 12.6 MB (69.51%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         
-        vector<int>dp(400);
+        vector<int>dp(366);
 
-        for(int i = 1;i<=370;i++){
+        for(int i = 1;i<=365;i++){
             if(find(days.begin(),days.end(),i)!=days.end()){
                 dp[i] = min({dp[i-1]+costs[0] , dp[max(0,i-7)]+costs[1],dp[max(0,i-30)]+costs[2]});
             }else{
