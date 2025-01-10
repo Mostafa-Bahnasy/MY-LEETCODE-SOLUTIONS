35aed7a009bef90dfb1406a2f18043cec005dfb1
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2025-01-10 06:11:15 +0200

    Time: 0 ms (100%), Space: 20.7 MB (73.2%) - LeetHub

---
+++ 0879-maximize-distance-to-closest-person/0879-maximize-distance-to-closest-person.cpp
@@ -0,0 +1,29 @@
+struct TinyInt {
+    unsigned int value :15  = 0; // 3 bits, can store values 0-7
+};
+
+
+
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        TinyInt c;
+        int ans;
+        bool is = true;
+        for(auto&i:seats){
+            if(i){
+                if(!is)
+                    ans = max(ans,(c.value+1)/2);
+                else
+                    ans = max(ans,1*c.value);
+
+                c.value=0;
+                is = false;
+            }else{
+                c.value++;
+            }
+        }
+        ans = max(ans,1*c.value);
+        return ans;
+    }
+};
