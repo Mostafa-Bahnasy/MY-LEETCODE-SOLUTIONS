077871acb69776c63272d33bba9e0e65e34c0aab
MustafaBahnasy <101218002+Mostafa-Bahnasy@users.noreply.github.com>
2025-01-10 03:38:53 +0200

    Time: 39 ms (72.15%), Space: 84.3 MB (72.15%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string>ret;
+
+        int cnt[26] = {};
+
+        for(auto&s:words2){
+            int cur[26] = {};
+            for(auto ch:s){
+                cur[ch-'a']++;
+            }
+            for(int i = 0;i<26;i++){
+                cnt[i] = max(cnt[i],cur[i]);
+            }
+        }
+
+        auto get =[&](string &s)->vector<int>{
+            vector<int>ret(26);
+            for(auto&i:s){
+                ret[i-'a']++;
+            }
+            return ret;
+        };
+
+        for(auto&s:words1){
+            auto cur = get(s);
+
+            bool is = true;
+            for(int i = 0;i<26;i++){
+                if(cur[i]<cnt[i])is = false;
+            }
+            if(is)ret.push_back(s);
+        }
+        return ret;
+    }
+};
